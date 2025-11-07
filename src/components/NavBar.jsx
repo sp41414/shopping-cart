df671306ea60a54cf5910202094be2d31ab9bcf4
sp41414 <134581084+sp41414@users.noreply.github.com>
2025-11-08 03:49:30 +0400

    feat: navbar styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,18 @@
 import { Link } from "react-router"
+import styles from "../styles/NavBar.module.css"
 
 const NavBar = ({ cartItems }) => {
-    return (
-        <div>
-            <h1>Shopping Cart</h1>
-            <div>
-                <Link to="/">Home</Link>
-                <Link to="/shop">Shop</Link>
-                <Link to="/cart">Cart</Link>
-                <p>{cartItems.length}</p>
-            </div>
-        </div>
-    )
+	return (
+		<div className={styles.container} >
+			<h1>Shopping Cart</h1>
+			<div className={styles.links}>
+				<Link to="/" className={styles.link}>Home</Link>
+				<Link to="/shop" className={styles.link}>Shop</Link>
+				<Link to="/cart" className={styles.link}>Cart</Link>
+				<p>Items: {cartItems.length}</p>
+			</div>
+		</div >
+	)
 }
 
 export default NavBar
